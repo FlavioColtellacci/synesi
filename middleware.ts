2653150c691a6886d37b2eede236f250bf15dc14
Flavio Coltellacci <coltellaccif@gmail.com>
2026-03-09 15:13:32 +0100

    feat: implement middleware and update global styles for improved branding

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,10 @@
-// TODO: implement
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/app/:path*'],
+}
